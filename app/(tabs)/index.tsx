20c3fc0efd2c844446f51d12d722cb93e90b87ef
Miya11111 <sub011256@gmail.com>
2025-12-12 22:28:33 +0900

    スワイプした時の更新アイコンの追加

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import { ScrollView } from 'react-native';
+import { useState } from 'react';
+import { RefreshControl, ScrollView } from 'react-native';
 
 import Tweet, { TweetType } from '@/components/organisms/tweet';
+import { useColors } from '@/hooks/use-colors';
 
 export const HomeLangJa = {
   welcome: 'こんにちは',
@@ -11,31 +13,92 @@ export const HomeLangEn = {
 };
 
 export default function HomeScreen() {
+  const colors = useColors();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    // 更新処理をシミュレート（実際にはAPI呼び出しなど）
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1500);
+  };
+
   const tweets: TweetType[] = [
     {
       image: require('@/assets/mock_icon1.png'),
       name: 'ミジンコ2',
       nameId: 'mijinji_minji',
-      message: 'あああああああああああああああああああああああああああ',
-      // animalNum: 1,
-      // retweetNum: 2,
-      // favoriteNum: 3,
-      // impressionNum: 10,
+      message: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
     },
     {
       image: require('@/assets/mock_icon1.png'),
       name: 'ユーザー名',
       nameId: 'user_id',
       message: 'サンプルツイートメッセージです',
-      // animalNum: 5,
-      // retweetNum: 3,
-      // favoriteNum: 8,
-      // impressionNum: 25,
+    },
+        {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ミジンコ2',
+      nameId: 'mijinji_minji',
+      message: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ユーザー名',
+      nameId: 'user_id',
+      message: 'サンプルツイートメッセージです',
+    },
+        {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ミジンコ2',
+      nameId: 'mijinji_minji',
+      message: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ユーザー名',
+      nameId: 'user_id',
+      message: 'サンプルツイートメッセージです',
+    },
+        {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ミジンコ2',
+      nameId: 'mijinji_minji',
+      message: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ユーザー名',
+      nameId: 'user_id',
+      message: 'サンプルツイートメッセージです',
+    },
+        {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ミジンコ2',
+      nameId: 'mijinji_minji',
+      message: 'あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ',
+    },
+    {
+      image: require('@/assets/mock_icon1.png'),
+      name: 'ユーザー名',
+      nameId: 'user_id',
+      message: 'サンプルツイートメッセージです',
     },
   ];
 
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          tintColor={colors.blue}
+          colors={[colors.blue]}
+          progressBackgroundColor={colors.white}
+        />
+      }
+    >
       {/* つぶやきタブ */}
       {tweets.map((tweet, key) => (
         <Tweet
